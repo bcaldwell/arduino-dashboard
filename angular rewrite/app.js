8d5c08629f172a98171a935034494afea1e2ccdc
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2015-02-17 21:21:49 -0500

    inline highchairs to button and updated layout

--- angular rewrite/app.js
+++ angular rewrite/app.js
@@ -1,10 +1,10 @@
 (function () {
-    var app = angular.module('arduino', ['angular.filter']);
+    var app = angular.module('arduino', ['angular.filter', 'highcharts-ng', 'ui.bootstrap']);
 
     app.controller('addPinController', ['pinFactory', function (pinFactory) {
         var that = this;
         this.pins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-        this.type = ["Digial Write", "Digital Read"]
+        this.type = ["Digital Write", "Digital Read"]
 
         this.selectClass = function () {
             if (that.selectedPin && that.selectedType) {
@@ -26,7 +26,7 @@
         };
     }]);
 
-    app.controller('pinController', ['pinFactory', function (pinFactory) {
+    app.controller('pinController', ['$scope', 'pinFactory', function ($scope, pinFactory) {
         this.pins = pinFactory.pins;
 
         this.selectClass = function (pin) {
@@ -43,7 +43,67 @@
         this.count = function () {
             return Object.keys(this.pins).length;
         }
+
+        this.chartConfig = {
+            options: {
+                chart: {
+                    type: 'solidgauge',
+                    backgroundColor: 'rgba(0,0,0,0)'
+                },
+                pane: {
+                    center: ['50%', '85%'],
+                    size: '180%',
+                    startAngle: -90,
+                    endAngle: 90,
+                    background: {
+                        backgroundColor: 'rgba(0,0,0,0)',
+                        innerRadius: '60%',
+                        outerRadius: '100%',
+                        shape: 'arc'
+                    }
+                },
+                solidgauge: {
+                    dataLabels: {
+                        enabled: false,
+                    }
+                },
+                tooltip: {
+                    enabled: false
+                },
+            },
+            series: [{
+                data: [6],
+                dataLabels: {
+                    format: '{y}',
+                    enabled: false
+                }
+                }],
+            title: {
+                text: 'Solid Gauge',
+            },
+            yAxis: {
+                currentMin: 0,
+                currentMax: 20,
+                title: {
+                    y: 140
+                },
+                stops: [
+                        [0.1, '#DF5353'], // red
+                  [0.5, '#DDDF0D'], // yellow
+                  [0.9, '#55BF3B'] // green
+           ],
+                lineWidth: 0,
+                tickInterval: 20,
+                tickPixelInterval: 400,
+                tickWidth: 0,
+                labels: {
+                    y: 15
+                }
+            },
+            loading: false
+        }
     }]);
+
 
     app.factory('pinFactory', function () {
         return {
