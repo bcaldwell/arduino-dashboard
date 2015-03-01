0e49f84a1543749922c2c6b0a405345d084e5fe7
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2015-03-01 12:47:10 -0500

    new readme file

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Node-Arduino
 Use a web interface to control an arduino connect to the server
+
+## Getting Started
+
+To get you started you can simply clone the node-arduino repository, install the dependencies and run the server:
+
+### Prerequisites
+
+You need git to clone the node-arduino repository. You can get git from
+[http://git-scm.com/](http://git-scm.com/).
+
+We also use a number of node.js tools to initialize and test node-arduino. You must have node.js and
+its package manager (npm) installed.  You can get them from [http://nodejs.org/](http://nodejs.org/).
+
+### Clone node-arduino
+
+Clone the node-arduino repository using [git][git]:
+
+```
+git clone https://github.com/benjamincaldwell/node-arduino.git
+cd node-arduino
+```
+
+### Install Dependencies
+
+* We get the server tools we depend upon via `npm`, the [node package manager][npm].
+* We get the browser code via `bower`, a [client-side code package manager][bower].
+
+We have preconfigured `npm` to automatically run `bower` so we can simply do:
+
+```
+npm install
+
+### Run the Application
+
+```
+npm start
+```
+
---
+++ public/app.js
@@ -0,0 +1,186 @@
+(function () {
+    var app = angular.module('arduino', ['angular.filter', 'highcharts-ng', 'ui.bootstrap']);
+
+    app.controller('addPinController', ['pinFactory', 'socket', function (pinFactory, socket) {
+
+        socket.on('test', function (data) {
+            console.log(data);
+        });
+
+        var that = this;
+        this.pins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+        this.type = ["Digital Write", "Digital Read"]
+
+        this.selectClass = function () {
+            if (that.selectedPin && that.selectedType) {
+                return false;
+            }
+            return true;
+        }
+
+        this.addPin = function () {
+            socket.emit('new pin', {
+                pin: that.selectedPin,
+                type: that.selectedType
+            }, function (state) {
+                console.log("log" + state);
+                pinFactory.pins[that.selectedPin] = {
+                    pin: that.selectedPin,
+                    status: state,
+                    type: that.selectedType,
+                    id: ++pinFactory.id
+                };
+            });
+        };
+    }]);
+
+    app.controller('pinController', ['pinFactory', 'socket', function (pinFactory, socket) {
+        this.pins = pinFactory.pins;
+
+        this.selectClass = function (pin) {
+            if (pin.status) {
+                return "btn-success";
+            };
+            return "btn-danger btn-disabled";
+        }
+
+        this.toggle = function (pin) {
+            //            pin.status = pin.status * (-1) + 1;
+            socket.emit('digitalWrite', pin.pin);
+        };
+
+        this.count = function () {
+            return Object.keys(this.pins).length;
+        }
+    }]);
+
+    app.controller('highchartsController', ['$scope', 'pinFactory', function ($scope, pinFactory) {
+
+        var that = this;
+        this.init = function (pin) {
+            that.pin = pin;
+
+            $scope.$watch(function () {
+                if (that.solidGaugeSmall.getHighcharts().series[0]) {
+                    var chart = that.solidGaugeSmall.getHighcharts();
+                    var point = chart.series[0].points[0];
+                    point.update(that.pin.status);
+                }
+                return that.pin.status;
+            }, function (newValue, oldValue) {
+                console.log(that.pin.status);
+                if (that.solidGaugeSmall.getHighcharts().series[0]) {
+                    var chart = that.solidGaugeSmall.getHighcharts();
+                    var point = chart.series[0].points[0];
+                    point.update(newValue);
+                }
+            });
+        }
+
+        this.solidGaugeSmall = {
+            options: {
+                chart: {
+                    type: 'solidgauge',
+                    backgroundColor: 'rgba(0,0,0,0)'
+
+                },
+                title: null,
+                pane: {
+                    center: ['50%', '85%'],
+                    size: '160%',
+                    startAngle: -75,
+                    endAngle: 75,
+                    background: {
+                        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+                        innerRadius: '60%',
+                        outerRadius: '100%',
+                        shape: 'arc'
+                    }
+                },
+                tooltip: {
+                    enabled: false
+                },
+            },
+            yAxis: {
+                min: 0,
+                max: 1,
+                stops: [
+                [0, '#DF5353'], // green
+                [1, '#55BF3B'] // red
+                ],
+                lineWidth: 0,
+                minorTickInterval: null,
+                tickPixelInterval: 400,
+                tickWidth: 0,
+                title: {
+                    text: null,
+                    y: -70
+                },
+                labels: {
+                    y: 16
+                }
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: null,
+                data: (function () {
+                    console.log(that.pin);
+                    return [0];
+                })(),
+                dataLabels: {
+                    enabled: false,
+                }
+        }],
+            plotOptions: {
+                solidgauge: {
+                    dataLabels: {
+                        enabled: false
+                    }
+                }
+            },
+            loading: false
+        }
+    }]);
+
+    app.factory('pinFactory', ['socket', function (socket) {
+        var pinFactory = {
+            id: 0,
+            pins: {}
+        }
+
+        socket.on('digital:change', function (data) {
+            if (pinFactory.pins[data.pin]) {
+                pinFactory.pins[data.pin].status = data.status;
+            };
+        });
+
+        return pinFactory;
+    }]);
+
+    app.factory('socket', function ($rootScope) {
+        var socket = io.connect();
+        return {
+            on: function (eventName, callback) {
+                socket.on(eventName, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        callback.apply(socket, args);
+                    });
+                });
+            },
+            emit: function (eventName, data, callback) {
+                socket.emit(eventName, data, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback.apply(socket, args);
+                        }
+                    });
+                })
+            }
+        };
+    });
+
+})();
